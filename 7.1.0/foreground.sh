e6b64d6945d4118e41f16e84680c8fb4d1d11e5c
Francesco Bianco <info.francescobianco@gmail.com>
2018-06-19 14:06:09 +0200

    fix me

--- 7.1.0/foreground.sh
+++ 7.1.0/foreground.sh
@@ -4,9 +4,11 @@
 CHECK=$(php foreground-check.php)
 case ${CHECK} in
     MYSQL_IMPORT_BY_ROOT)
-        mysql -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < vtigercrm.sql ;;
+        echo '>>>>>> Vtiger CRM import database by root.'
+        mysql -h${MYSQL_HOST} -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_DATABASE} < vtigercrm.sql ;;
     MYSQL_IMPORT_BY_USER)
-        mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < vtigercrm.sql ;;
+        echo '>>>>>> Vtiger CRM import database by user.'
+        mysql  -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < vtigercrm.sql ;;
     READY)
         echo '>>>>>> Vtiger CRM is ready.' ;;
     *)
