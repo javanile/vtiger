170b911944d8def4feb0fa1e5559c85ebf245ac9
francesco <info.francescobianco@gmail.com>
2019-05-02 15:51:19 +0200

    This was published on Thu May  2 15:51:19 CEST 2019

--- 6.0.0-Beta/vtiger-foreground.sh
+++ 6.0.0-Beta/vtiger-foreground.sh
@@ -11,7 +11,7 @@ echo "   --------${VT_VERSION}-   " | sed 's/[^ ]/‾/g'
 printenv | sed 's/^\(.*\)$/export \1/g' | grep -E '^export MYSQL_|^export VT_' > /etc/env.sh
 
 ## import database using environment variables
-echo "[vtiger] starting up...\n";
+echo "[vtiger] starting up...";
 cd /var/www/html/ && mysql-import vtiger.sql && php vtiger-startup.php
 
 ## update permissions
--- publish.sh
+++ publish.sh
@@ -8,5 +8,5 @@ docker-compose run --rm update
 [[ -d ./volumes ]] && rm -fr ./volumes
 
 git add .
-git commit -am "publish"
+git commit -am "This was published on $(date)"
 git push
