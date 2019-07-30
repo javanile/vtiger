ac419356c9dd290da441d6d7cde4449a82e3cba4
Francesco Bianco <info.francescobianco@gmail.com>
2019-07-31 00:53:37 +0200

    minor fix

--- 6.0.0/vtiger-foreground.sh
+++ 6.0.0/vtiger-foreground.sh
@@ -11,22 +11,22 @@ echo "   --------${VT_VERSION}-   " | sed 's/[^ ]/‾/g'
 printenv | sed 's/^\(.*\)$/export \1/g' | grep -E '^export MYSQL_|^export VT_' > /run/crond.env
 
 ## import database using environment variables
-echo "[vtiger] starting up...";
+echo "[vtiger] Starting up...";
 cd /usr/src/vtiger && echo -n "[vtiger] " && mysql-import vtiger.sql && php vtiger-startup.php
 
 ## update permissions
-echo "[vtiger] prepare log files"
+echo "[vtiger] Prepare log files"
 cd /var/www/html/logs
 touch access.log apache.log migration.log platform.log soap.log php.log
 touch cron.log installation.log security.log sqltime.log vtigercrm.log
 
 ## return to working directory
-echo "[vtiger] set working directory: ${WORKDIR}"
+echo "[vtiger] Set working directory: ${WORKDIR}"
 cd ${WORKDIR}
 
 ## copy vtiger.json file on working directory
 [[ ! -f vtiger.json ]] && cp /usr/src/vtiger/vtiger.json .
 
 ## run cron and apache
-echo "[vtiger] launch foreground process..."
+echo "[vtiger] Launch foreground process..."
 cron && apache2-foreground
