429add24dc656c3419eac9abd9de91145ad3c834
Francesco Bianco <info.francescobianco@gmail.com>
2019-10-10 22:29:57 +0200

    fix autoload

--- 6.0.0-Beta/vtiger-autoload.php
+++ 6.0.0-Beta/vtiger-autoload.php
@@ -1,12 +1,12 @@
 <?php
 
-define('DB_HOST', $_ENV['MYSQL_HOST'] ?: 'mysql');
-define('DB_PORT', $_ENV['MYSQL_PORT'] ?: '3306');
-define('DB_NAME', $_ENV['MYSQL_DATABASE'] ?: 'vtiger');
-define('DB_USER', $_ENV['MYSQL_USER'] ?: 'root');
-define('DB_PASS', $_ENV['MYSQL_PASSWORD'] ?: ($_ENV['MYSQL_ROOT_PASSWORD'] ?: 'root'));
-define('DB_ROOT', $_ENV['MYSQL_ROOT_PASSWORD'] ?: 'root');
+define('DB_HOST', getenv('MYSQL_HOST') ?: 'mysql');
+define('DB_PORT', getenv('MYSQL_PORT') ?: '3306');
+define('DB_NAME', getenv('MYSQL_DATABASE') ?: 'vtiger');
+define('DB_USER', getenv('MYSQL_USER') ?: 'root');
+define('DB_PASS', getenv('MYSQL_PASSWORD') ?: (getenv('MYSQL_ROOT_PASSWORD') ?: 'root'));
+define('DB_ROOT', getenv('MYSQL_ROOT_PASSWORD') ?: 'root');
 
 date_default_timezone_set('America/Los_Angeles');
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once '/root/.composer/vendor/autoload.php';
