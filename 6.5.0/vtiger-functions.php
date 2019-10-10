ba5a593c97939ad22bd4095609153552ad266239
Francesco Bianco <info.francescobianco@gmail.com>
2019-10-11 01:52:14 +0200

    fix vtiger functions #2

--- 6.5.0/vtiger-functions.php
+++ 6.5.0/vtiger-functions.php
@@ -1,33 +1,28 @@
 <?php
 
 /**
+ * Retrieve realpath without resolve symbolic link.
+ *
  * @param $path
  */
-function __realpath($path)
+function __realpath($path, $link = null)
 {
+    if ($link) {
+        $head = trim(substr($path, strlen($link)), '/');
+        $base = basename($link);
+        $real = __realpath(dirname($link));
+
+        return $real.'/'.$base.'/'.$head;
+    }
+
     $link = $path;
     while (!is_link($link) && $link != '.' && $link != '/') {
         $link = dirname($link);
     }
 
     if ($link != '.' && $link != '/') {
-        return __realpath_link($path, $link);
+        return __realpath($path, $link);
     }
 
     return realpath($path);
-}
-
-/**
- * @param $path
- * @param $link
- * @return bool|string
- */
-function __realpath_link($path, $link)
-{
-    $dir = dirname($link);
-    $cat = trim(substr($path, strlen($link)), '/');
-    $base = basename($link);
-    $real = __realpath($dir);
-
-    return $real.'/'.$base.'/'.$cat;
 }
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php require_once "/usr/src/vtiger/vtiger-functions.php";
-include 'vtiger-functions.php';
-
-echo ____realpath('php.ini')."\n";
-echo ____realpath('../vtiger/demo/vtiger-demo/docker-compose.yml')."\n";
-echo ____realpath('../vtiger/demo/vtiger-demo/.idea/inspectionProfiles/')."\n";
