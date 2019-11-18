70148a53e5c6fdf7430c56ed8a1da7a2ada474ea
Francesco Bianco <info.francescobianco@gmail.com>
2019-11-18 12:44:23 +0100

    small fix

--- 7.0.0/vtiger-foreground.sh
+++ 7.0.0/vtiger-foreground.sh
@@ -14,8 +14,9 @@ touch access.log apache.log migration.log platform.log soap.log php.log
 touch cron.log installation.log security.log sqltime.log vtigercrm.log
 
 ## run apache for debugging
+echo "[vtiger] Starg web loading..."
 cp -f /var/www/html/index.php /var/www/html/index.php.0
-debug() { sed -e 's!%%MESSAGE%%!'"$1"'!' /var/www/html/loading.php > /var/www/html/index.php; }
+loading() { sed -e 's!%%MESSAGE%%!'"$1"'!' /var/www/html/loading.php > /var/www/html/index.php; }
 service apache2 start >/dev/null 2>&1
 
 ## store environment variables
@@ -23,13 +24,13 @@ printenv | sed 's/^\(.*\)$/export \1/g' | grep -E '^export MYSQL_|^export VT_' >
 
 ## import database using environment variables
 cd /usr/src/vtiger
-debug "Waiting for database preparation..."
+loading "Waiting for database..."
 echo "[vtiger] Waiting for available database..."
 echo -n "[vtiger] " && mysql-import --do-while vtiger.sql
 #php vtiger-startup.php
 
 ## fill current mounted volume
-debug "Waiting for volume preparation..."
+loading "Waiting for volume preparation..."
 echo "[vtiger] Waiting for preparation volume: /var/lib/vtiger"
 symvol copy /usr/src/vtiger/volume /var/lib/vtiger && symvol mode /var/lib/vtiger www-data:www-data
 symvol link /var/lib/vtiger /var/www/html && symvol mode /var/www/html www-data:www-data
@@ -48,7 +49,7 @@ cd ${WORKDIR}
 [[ ! -f vtiger.json ]] && cp /usr/src/vtiger/vtiger.json .
 
 ## run cron and apache
-echo "[vtiger] Launch foreground process..."
+echo "[vtiger] Run main process..."
 service apache2 stop >/dev/null 2>&1
 cp -f /var/www/html/index.php.0 /var/www/html/index.php
 apache2-foreground
--- publish.sh
+++ publish.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-docker-compose run --rm update
-
-[[ -d ./volumes ]] && rm -fr ./volumes
+docker-compose run --rm debian ./update.sh
+docker-compose run --rm debian rm -fr ./vtiger
 
 git add . > /dev/null
 git commit -am "$*"
